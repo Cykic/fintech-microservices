3a7eab28e75e978d0751104e2f99ac0fc8e6ebee
cykic <cyb3rcykic@gmail.com>
2021-11-19 14:58:43 +0000

    Added test with jest and chai and supertest

--- services/accounts/packages.sh
+++ services/accounts/packages.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-npm i --save-dev cross-env eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-node eslint-plugin-prettier jest prettier supertest
+npm i --save-dev cross-env eslint chai chai-http eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-node eslint-plugin-prettier jest prettier supertest
 npm i body-parser cookie-parser compression cors dotenv express express-mongo-sanitize express-rate-limit helmet mongoose morgan validator xss-clean
 npm fund
 npm audit
---
+++ services/customers/test/customers.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/* eslint-disable node/no-unpublished-require */
+const request = require('supertest');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const server = require('../server');
+
+const app = require('../app');
+
+describe('Tour Routes', () => {
+  it('Should get all route', async () => (done) => {
+    chai
+      .request(server)
+      .get('/api/v1/customers')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+
+  it('Should return 404', async () => {
+    const res = await request(app).get('*');
+    expect(res.statusCode).toEqual(404);
+  });
+});
